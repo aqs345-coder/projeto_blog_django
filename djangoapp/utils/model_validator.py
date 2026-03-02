6e96abeebc2a864add3f1c6b0576eaaea107cb84
aqs_coder <aqsuel345@gmail.com>
2026-03-02 00:43:20 -0300

    Aula 534 Montando a base do CSS

---
+++ djangoapp/utils/model_validator.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+
+
+def validate_png(image):
+    if not image.name.lower().endswith('.png'):
+        raise ValidationError(
+            'Somente imagens PNG são aceitas.'
+        )
