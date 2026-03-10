82eed02332781c5c624c097fd733e65b45079527
aqs_coder <aqsuel345@gmail.com>
2026-03-10 20:52:22 -0300

    Aula 536-538, Usando o context processor do model site_setup dentro dos templates para config

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,7 +1,8 @@
-from blog.views import index
+from blog.views import index, page, post
 from django.urls import path
 
 urlpatterns = [
     path('', index, name='index'),
+    path('page/', page, name='page'),
+    path('post/', post, name='post'),
 ]
-
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'blog/pages/index.html')
+
+
+def post(request):
+    return render(request, 'blog/pages/post.html')
+
+
+def page(request):
+    return render(request, 'blog/pages/page.html')
